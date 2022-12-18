fb94f1a3124fc319a2c02b895d3e36385051507a
Nikkolla <arebanikko@gmail.com>
2022-12-18 05:30:19 +0400

    no comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
+import React, { useState } from "react";
 
 
-function App() {
+export default function App() {
+  const [currentItemValue, setCurrentItemValue] = useState("");
+  const [items, setItems] = useState([]);
+
+  function addItem() {
+    setItems([...items, currentItemValue]);
+    setCurrentItemValue("");
+  }
 
   return (
     <div>
-      <div>
+      <div className="bg-black h-screen ">
+        <div className="flex justify-center">
+          <input
+            type="text"
+            value={currentItemValue}
+            className="mt-16 w-96 py-1 px-3 outline-none shadow-slate-800 shadow-md rounded-lg bg-slate-700 text-white border border-solid border-transparent"
+            placeholder="Take a note..."
+            onChange={(e) => {
+              setCurrentItemValue(e.target.value);
+            }}
+          />
+          <button
+            onClick={(e) => {
+              addItem(currentItemValue);
+            }}
+            className="text-white ml-4 mt-16"
+          >
+            Add
+          </button>
+        </div>
+        <div className="grid justify-center text-white mt-10 grid-cols-6">
+          {items.map((i) => {
+            return <div className="p-4 border border-solid border-gray-100 m-10 pb-10 rounded-xl">{i}</div>;
+          })}
+        </div>
       </div>
     </div>
-  )
+  );
 }
-
-export default App
